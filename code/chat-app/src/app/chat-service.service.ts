156302ea18da630ebe7dcd02f8e279525b9c8dd1
vidurukalpana <mwvidurangikalpana@gmail.com>
2026-09-17 15:54:31 +0530

    refactor

--- code/chat-app/src/app/chat-service.service.ts
+++ code/chat-app/src/app/chat-service.service.ts
@@ -1,31 +1,19 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Answer} from "./models/Answer";
+import { HttpClient } from '@angular/common/http';
+import { Answer } from './models/Answer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
+  backendBaseUrl: string = 'http://localhost:5000/';
 
-  backendBaseUrl: string = 'http://localhost:5000/'
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getAnswer(question: string) {
-
-
-    // return this.http.post<string>(
-    //   this.backendBaseUrl + 'process',
-    //   {data: question},
-    //   {
-    //     observe: 'response',
-    //   });
-
     return this.http.post<Answer>(
       this.backendBaseUrl + 'api',
-      {question: question, category: "MP"})
-      // {responseType: 'text'})
-
+      { question, category: 'MP' }
+    );
   }
-
 }
